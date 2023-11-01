ade74dab3d5d94f05fd39450df25ff694dfeb3ff
HuHu <hongbo.hhu318@gmail.com>
2023-10-31 20:56:52 -0400

    update signup and signin

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,19 +4,29 @@ const jwt = require('jsonwebtoken');
 
 exports.signUp = async(req, res) => {
     const { username, password } = req.body;
-    let user = await user.findOne({ username });
-    if (user) {
+    let newUser = await user.findOne({ username });
+    if (newUser) {
         return res.status(400).json({ msg: 'user already exists'});
     }
-    user = new User({
-        username, password: bcrypt.hashSync(passowrd, 10)
+    newUser = new user({
+        username, password: bcrypt.hashSync(password, 10)
     });
-    await user.save();
+    await newUser.save();
     const token = jwt.sign({ id: user.id}, process.env.SECRET_KEY, { expiresIn: '2h'});
-    return res.status(200).json({ token, user });
+    return res.status(200).json({ token, newUser });
 }
 
 exports.signIn = async(req, res) => {
     console.log('req body: ' + req.body);
     const { username, password } = req.body;
+    let user = await user.findOne({ username });
+    if (!user) {
+        return res.status(400).json({ msg: 'user does not exists'});
+    }
+    const isMatch = bcrypt.compareSync(password, user.passowrd);
+    if (!isMathch) {
+        return res.status(400).json({ msg: 'incorrect passowrd, please try again'});
+    }
+    const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY, { expiresIn: '2h'});
+    return res.status(200).json({ token, user });
 }
